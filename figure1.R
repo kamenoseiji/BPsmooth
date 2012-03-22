3588261a7f2784eea95741850f0c85edbca6e841
Seiji Kameno <kameno@sci.kagoshima-u.ac.jp>
2012-03-22 11:55:42 +0900

    modified:   README 	modified:   figure1.R 	modified:   figure1.pdf 	new file:   figure2.pdf 	new file:   figure3.pdf 	new file:   figure4.pdf 	new file:   figure5.pdf 	new file:   figure6.pdf 	new file:   figure7.pdf 	new file:   figure8.pdf

--- figure1.R
+++ figure1.R
@@ -4,30 +4,38 @@ spec36000 <- matrix(readBin("data_36000.dat",what=numeric(),n=1024*36000,size=4,
 spec36000[,22466] <- (spec36000[,22465] + spec36000[,22467])/2	# Flag out irregular scan
 spec36000[,22464] <- (spec36000[,22463] + spec36000[,22465])/2	# Flag out irregular scan
 spec36000[,22462] <- (spec36000[,22461] + spec36000[,22463])/2	# Flag out irregular scan
+cat(sprintf("Template bandpass and spectral data are loaded.\n"))
 # Template bandpassを作成して補正
 tempspec <- rowSums(spec230)/230
 caled32768 <- spec36000[,3231:35998]/tempspec
+cat(sprintf("Template bandpass calibration, applied.\n"))
 # Prepare a PDF to plot figure 1
+cat(sprintf("Start processing for figure 1.\n"))
 pdf("figure1.pdf")
+cat(sprintf("-- Integration and smoothing the spectra.\n"))
 spec546 <- t(apply(spec36000[,3239:35998], 1, bunch, lag=60))
 caled32760 <- spec36000[,3239:35998]/tempspec
 caled546 <- t(apply(caled32760, 1, bunch, lag=60))
 spline546 <- smooth.spline(rowSums(caled546[2:1024,])/546, nknots=1024 %/% 42)
 plot(allanvar(tail(spec546[,256],1023)), log='xy', xlim=c(1,546), ylim=c(5e-14, 5e-6), type='l', xlab='Channel Separation [ch]', ylab='Allan Variance')
+cat(sprintf("-- Calculating Allan Variance.\n"))
 lines(allanvar(tail(caled546[,256],1023)), col='red')
 lines(allanvar(tail(rowSums(caled546)/546,1023)), col='blue')
 lines(allanvar(predict(spline546, 1:1023)$y), col='green')
 dev.off()
 # Prepare a PDF to plot figure 2
-pdf("figure1.pdf")
+cat(sprintf("Start processing for figure 2.\n"))
 pdf("figure2.pdf")
+cat(sprintf("-- Plot the template spectrum.\n"))
 plot(tempspec[2:1024], type='s', ylim=c(0,1.2), xlab='Frequency [ch]', ylab='Normalized Amplitude')
 dev.off()
 # Prepare a PDF to plot figure 3
+cat(sprintf("Start processing for figure 3.\n"))
 pdf("figure3.pdf")
 caled10sec <- t(apply(caled32760[,1:32400], 1, bunch, lag=10))    # 10秒ごとにスペクトルを時間積分。合計3240スキャン
 spline10sec <- matrix(nrow=nrow(caled10sec), ncol=ncol(caled10sec))
 node_ch <- 45	# 45-ch窓で平滑化
+cat(sprintf("-- SBC applied for 10-sec integrated spectra.\n"))
 for(scan_index in 1:ncol(caled10sec)){
 	spline10sec[,scan_index] <- predict(smooth.spline(caled10sec[2:1024, scan_index], all.knots=TRUE, df=(1024 %/% node_ch)), 1:1024)$y # OFF点スペクトルを周波数方向に平滑化
 }
@@ -35,20 +43,23 @@ plot(caled10sec[, 601], pch=20, ylim=c(1.02, 1.045), xlab='Frequency [ch]', ylab
 lines(spline10sec[,600], col='red', lwd=2)
 dev.off()
 # Prepare a PDF to plot figure 4
+cat(sprintf("Start processing for figure 4.\n"))
 pdf("figure4.pdf")
+cat(sprintf("-- Calculating Allan Variances with various integration time (1, 2, 4, 8, …, 32768 sec).\n"))
 av32768 <- allanvar_period(caled32768, c(2^(0:15)))
 plot(av32768[,1], log='xy', pch=20, cex=0.4, ylim=c(1.0e-11, 1.0e-5), type='l', xlab='Channel Separation [ch]', ylab='Allan Variance')
-for(i in 1:15){lines(av32768[,i]}
+for(i in 1:15){lines(av32768[,i])}
 dev.off()
 # Prepare a PDF to plot figure 5
+cat(sprintf("Start processing for figure 5.\n"))
 pdf("figure5.pdf")
 caled1min <- t(apply(caled32760, 1, bunch, lag=60))    # スペクトルを60秒ずつ積分して1min x 546 segmentsのデータにする
 av512min <- allanvar_period( caled1min, 2^(0:9)) # 1, 2, 4, 8, …, 512 min積分したスペクトルのアラン分散
-for(i in 1:10){bottom[i] <- min(av512min[1:200,i]); bottom_ch[i] <- which(av512min[,i] == min(av512min[1:200,i]))} # AVの底打ちを積分時間ごとに捉える
-btm <- data.frame(integ=2^(0:9), ch=bottom_ch, av=bottom) # AVの底打ちデータフレーム
-plot(btm$integ, btm$ch, log='xy', pch=19, xlab='Integration time [min]', ylab='CH separation at the minimum AV [CH]') # AV底打ち周波数を積分時間に対してプロット
-fit <- nls(formula = ch ~ a* integ^b, data=btm, start=c(a=130, b=-0.3), weight=c(rep(1,7), rep(0,3))) # AV底打ち周波数を積分時間に対してpower lawでフィット。重みは最初の7点だけを用い、t≧128 minは不使用。
-lines(btm$integ, predict(fit, btm$integ)) # power lawをプロット。power index = -0.273±0.005
+#for(i in 1:10){bottom[i] <- min(av512min[1:200,i]); bottom_ch[i] <- which(av512min[,i] == min(av512min[1:200,i]))} # AVの底打ちを積分時間ごとに捉える
+#btm <- data.frame(integ=2^(0:9), ch=bottom_ch, av=bottom) # AVの底打ちデータフレーム
+#plot(btm$integ, btm$ch, log='xy', pch=19, xlab='Integration time [min]', ylab='CH separation at the minimum AV [CH]') # AV底打ち周波数を積分時間に対してプロット
+#fit <- nls(formula = ch ~ a* integ^b, data=btm, start=c(a=130, b=-0.3), weight=c(rep(1,7), rep(0,3))) # AV底打ち周波数を積分時間に対してpower lawでフィット。重みは最初の7点だけを用い、t≧128 minは不使用。
+#lines(btm$integ, predict(fit, btm$integ)) # power lawをプロット。power index = -0.273±0.005
 av64 <- data.frame(ch=1:220, av=av512min[1:220, 7]) # 64-min積分のAVを、ch 1 - 220で切り取ってデータフレームに格納
 fit <- nls(formula = av ~ a*ch^b + c*ch^d, data=av64, start=c(a=1e-7, b=-2, c=5e-13, d=1.35)) # 2成分のpower lawでフィット
 #> summary(fit)
@@ -67,4 +78,34 @@ fit <- nls(formula = av ~ a*ch^b + c*ch^d, data=av64, start=c(a=1e-7, b=-2, c=5e
 #Achieved convergence tolerance: 1.308e-07 
 plot(av64, log='xy', pch=19, cex=0.4, xlab='Channel Separation [ch]', ylab='Allan Variance')
 lines(av64$ch, predict(fit, av64$ch), col='red')
+dev.off()
+# Prepare a PDF to plot figure 6
+cat(sprintf("Start processing for figure 6.\n"))
+pdf("figure6.pdf")
+caled30sec <- t(apply(caled32760, 1, bunch, lag=30))    # 30-sec integrated spectra
+spline30sec <- matrix(nrow=nrow(caled30sec), ncol=ncol(caled30sec))
+node_ch <- 45	# 45-ch窓で平滑化
+for(scan_index in 1:ncol(caled30sec)){
+	spline30sec[,scan_index] <- predict(smooth.spline(caled30sec[2:1024, scan_index], all.knots=TRUE, df=(1024 %/% node_ch)), 1:1024)$y # OFF点スペクトルを周波数方向に平滑化
+}
+persp(spline30sec, theta = -50, phi = 20, col = 'lightskyblue', d=2, xlab='Frequency [span = 16 MHz]', ylab='Time [span = 32768 sec]', zlab='Power', lwd=0.2, expand=0.2)
+dev.off()
+# Prepare a PDF to plot figure 7
+cat(sprintf("Start processing for figure 7.\n"))
+pdf("figure7.pdf")
+sd_spline10 <- apply(spline10sec, 1, sd)	# 平滑化したBPの時間方向の分散
+sd_spline30 <- apply(spline30sec, 1, sd)	# 平滑化したBPの時間方向の分散
+plot(sd_spline10, type='l', xlab='Frequench [ch]', ylab='Standard Deviation', main='Bandpass variability', col='red')
+dev.off()
+maxVar10 <- which.max(sd_spline10); minVar10 <- which.max(sd_spline10[1:200])
+maxVar30 <- which.max(sd_spline30); minVar30 <- which.max(sd_spline30[1:200])
+# Prepare a PDF to plot figure 8
+cat(sprintf("Start processing for figure 8.\n"))
+pdf("figure8.pdf")
+av10 <- allanvar((spline10sec[maxVar10,] - spline10sec[minVar10,])) / (10^2) #-------- 最も時間変動するchで時間方向のアラン分散
+av30 <- allanvar((spline30sec[maxVar30,] - spline30sec[minVar30,])) / (30^2) #-------- 最も時間変動するchで時間方向のアラン分散
+plot(10*(1:length(av10)), av10, log='xy', pch=19, xlab='Time Lag [sec]', ylab='Allan Variance', main='Spline-smoothed bandpass', xlim=c(10,3600), ylim=c(4e-11, max(av10)))
+lines(10*(1:length(av10)), av10)
+par(new=T); plot(30*(1:length(av30)), av30, log='xy', pch=0, xlab='', ylab='', main='', xlim=c(10,3600), ylim=c(4e-11, max(av10)))
+lines(30*(1:length(av30)), av30)
 dev.off()
